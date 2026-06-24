fbc18af96f688d9afca072680e27000738d4b884
TanKaizokuO <tanishqbhattacharjee22@gmail.com>
2026-06-24 17:18:34 +0530

    feat: implement argument parsing using mri

--- bin/aegis.js
+++ bin/aegis.js
@@ -1,3 +1,19 @@
 #!/usr/bin/env node
 
-console.log('Aegis CLI initialized');
+import mri from 'mri';
+
+const argv = process.argv.slice(2);
+const args = mri(argv, {
+  boolean: [
+    'version', 'help', 'stdin', 'text', 'html', 'mdx', 'diff', 'quiet', 'why'
+  ],
+  string: ['reporter'],
+  alias: {
+    v: 'version',
+    h: 'help',
+    q: 'quiet',
+    t: 'text',
+    r: 'reporter'
+  }
+});
+
